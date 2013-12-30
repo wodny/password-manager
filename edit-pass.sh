7d17ee54e567af32e71b4174d75f1f6d2dbf3aec
Marcin Szewczyk, Wodny <hq@wodny.org>
2013-12-30 11:47:03 +0100

    gpg to output to temporary file, not to source directly
    if gpg fails the source stays untouched
    be more verbose as well

--- edit-pass.sh
+++ edit-pass.sh
@@ -3,35 +3,50 @@
 sourcefile=${1:?Source file is required}
 sourcetype=${sourcefile##*.}
 
-tempfile=$(mktemp) || exit 1
-markfile=$(mktemp) || exit 2
+tempsource=$(mktemp) || exit 1
+tempdest=$(mktemp) || exit 2
+markfile=$(mktemp) || exit 3
 
 do_remove () {
-    [ -x /usr/bin/shred ] && shred -u "$tempfile" || rm -f "$tempfile"
+    if [ -x /usr/bin/shred ]; then
+        msg "Shreding temporary files"
+        shred -u "$tempsource"
+        shred -u "$tempdest"
+    else
+        msg "Removing temporary files"
+        rm -f "$tempsource"
+        rm -f "$tempdest"
+    fi
     rm -f "$markfile"
 }
 
+msg() {
+  echo ">>>" "$@"
+}
+
 do_error () {
-    echo "$1" >&2
+    msg "$1" >&2
     do_remove
     exit 3
 }
 
-echo "Decrypting $sourcefile of type $sourcetype to $tempfile"
-gpg -d < "$sourcefile" > "$tempfile" || do_error "Failed to decrypt sourcefile"
-editor "$tempfile"
-if [ "$tempfile" -nt "$markfile" ]; then
-    echo "Rewriting sourcefile"
+msg "Decrypting $sourcefile of type $sourcetype to $tempsource"
+gpg -d < "$sourcefile" > "$tempsource" || do_error "Failed to decrypt sourcefile"
+editor "$tempsource"
+if [ "$tempsource" -nt "$markfile" ]; then
+    msg "Rewriting sourcefile"
     if [ "$sourcetype" = "asc" ]; then
-        gpg --default-recipient-self -e -a < "$tempfile" > "$sourcefile"
+        gpg --default-recipient-self -e -a < "$tempsource" > "$tempdest"
     else
-        gpg --default-recipient-self -e < "$tempfile" > "$sourcefile"
+        gpg --default-recipient-self -e < "$tempsource" > "$tempdest"
     fi
     [ $? -ne 0 ] && do_error "Failed to reencrypt"
+    msg "Copying temporary encrypted file $tempsource to original source $sourcefile"
+    cp "$tempdest" "$sourcefile" || do_error "Failed to copy"
 else
-    echo "Edit aborted"
+    msg "Edit aborted"
 fi
 
 do_remove
 
-echo "Finished"
+msg "Finished"
